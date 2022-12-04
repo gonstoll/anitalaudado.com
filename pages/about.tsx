bea7c4af9ebe8c8f43f5b9090ef2c2c39353653d
Gonzalo Stoll <stollgonzalo@gmail.com>
2022-12-04 21:48:58 +0100

    chore: Turning off eslint escaped entities rule

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,7 +13,30 @@ export default function About() {
       >
         <div className="mt-20 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-6 items-center">
           <div>
-            <h2 className="mb-10 text-3-1/2xl text-black dark:text-white"></h2>
+            <h2 className="mb-10 text-3-1/2xl text-black dark:text-white">
+              I'm Ana, a Product and Visual designer specialised in UI and
+              Design Systems.
+            </h2>
+            <p className="text-base text-black dark:text-white">
+              Originally from Ushuaia, Argentina, also known as the southernmost
+              city in the world. At a young age I moved to the other side of the
+              country to follow my passion for art, illustration and design,
+              where I got my bachelor in Graphic Design. I've worked as a Visual
+              Designer and UI/UX Designer in some of the most recognizable
+              design and digital agencies.
+              <br />
+              <br />
+              Following that direction, a few years ago I sold everything and
+              moved to the complete opposite hemisphere to continue my
+              adventure. And that's exactly what I'm doing here in beautiful
+              Copenhagen, where I work as a Product Designer specialised in
+              Design Systems.
+              <br />
+              <br />
+              When I'm not designing or drawing I love experimenting in the
+              kitchen, growing my indoor jungle, exploring new places and trying
+              to catch some live music.
+            </p>
           </div>
         </div>
       </Layout>
