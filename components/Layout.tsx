fac6bd66705643a16e724ce58ba076671764111d
Gonzalo Stoll <stollgonzalo@gmail.com>
2022-12-04 17:20:07 +0100

    feat: Footer and copyright done

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,64 +11,56 @@ export default function Layout({children}: React.PropsWithChildren<object>) {
 
       <main className="mb-auto">{children}</main>
 
-      <footer className="p-10 border-t-1 border-black dark:border-white">
-        <div className="md:flex items-center justify-between mb-8">
-          <Link
-            href="/"
-            className="block text-3.5xl text-black dark:text-white"
-          >
-            ✦
-          </Link>
-          <Link
-            href="mailto:anitalaudado@gmail.com"
-            className="block text-2xl md:text-3.5xl font-bold text-black dark:text-white"
-          >
-            anitalaudado@gmail.com
-          </Link>
+      <footer className="border-t-1 border-black dark:border-white">
+        <div className="p-10">
+          <div className="md:flex items-center justify-between mb-8">
+            <Link
+              href="/"
+              className="block text-3.5xl text-black dark:text-white"
+            >
+              ✦
+            </Link>
+            <Link
+              href="mailto:anitalaudado@gmail.com"
+              className="block text-2xl md:text-3.5xl font-bold text-black dark:text-white"
+            >
+              anitalaudado@gmail.com
+            </Link>
+          </div>
+          <div className="md:flex items-end justify-between">
+            <div className="mb-8 md:mb-0">
+              <Tile size="small" title="Work" link="/work" />
+              <Tile size="small" title="Personal Projects" />
+              <Tile size="small" title="About" link="/about" />
+            </div>
+            <div className="flex flex-wrap gap-4">
+              <LinkButton
+                href="https://www.linkedin.com/in/ana-laudado/"
+                type="primary"
+                size="small"
+                title="linkedin"
+              />
+              <LinkButton
+                href="/docs/Ana_Laudado_2023.pdf"
+                type="primary"
+                size="small"
+                title="resume ↓"
+                target="_blank"
+                rel="noreferrer"
+              />
+              <LinkButton href="#" type="primary" size="small" title="top ↑" />
+            </div>
+          </div>
         </div>
-        <div className="md:flex items-end justify-between">
-          <div className="mb-8 md:mb-0">
-            <Tile size="small" title="Work" link="/work" />
-            <Tile size="small" title="Personal Projects" />
-            <Tile size="small" title="About" link="/about" />
-          </div>
-          <div className="flex flex-wrap gap-4">
-            <LinkButton
-              href="https://www.linkedin.com/in/ana-laudado/"
-              type="primary"
-              size="small"
-              title="linkedin"
-            />
-            <LinkButton
-              href="/docs/Ana_Laudado_2023.pdf"
-              type="primary"
-              size="small"
-              title="resume ↓"
-              target="_blank"
-              rel="noreferrer"
-            />
-            <LinkButton href="#" type="primary" size="small" title="top ↑" />
-            {/* <Link
-              className="h-8 px-4 flex items-center rounded-4xl border-1 border-black dark:border-white text-black dark:text-white"
-              href="https://www.linkedin.com/in/ana-laudado/"
-            >
-              linkedin
-            </Link>
-            <Link
-              className="h-8 px-4 flex items-center rounded-4xl border-1 border-black dark:border-white text-black dark:text-white"
-              href="/docs/Ana_Laudado_2023.pdf"
-              target="_blank"
-              rel="noreferrer"
-            >
-              resume ↓
-            </Link>
-            <Link
-              className="h-8 px-4 flex items-center rounded-4xl border-1 border-black dark:border-white text-black dark:text-white"
-              href="#"
-            >
-              top ↑
-            </Link> */}
-          </div>
+
+        <div className="copyright-bar px-10 py-6 md:flex items-center justify-between bg-black dark:bg-white">
+          <p className="text-base mb-6 md:mb-0 text-white dark:text-black">
+            Ana Laudado - {new Date().getFullYear()}
+          </p>
+          <p className="text-base uppercase text-white dark:text-black">
+            Made with love ✦ <br className="md:hidden" />
+            From the end of the world
+          </p>
         </div>
       </footer>
     </>
