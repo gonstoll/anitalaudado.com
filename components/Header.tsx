98660481692099ebe572d83136199e53ddbe6ae9
Gonzalo Stoll <stollgonzalo@gmail.com>
2022-12-05 20:31:23 +0100

    feat: Mobile menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,34 +53,69 @@ function useStickyHeader() {
   return {isVisible};
 }
 
+const links = [
+  {href: '/', title: 'Work'},
+  {href: '/about', title: 'About'},
+];
+
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const {theme, toggleTheme} = useTheme();
   const {isVisible} = useStickyHeader();
 
   const visibleClass = isVisible ? 'top-0' : 'top-0 -top-20';
+  const menuBtnClass = isMenuOpen
+    ? 'before:rotate-45 after:-rotate-45'
+    : 'before:-translate-y-1 after:translate-y-1';
+  const mobileMenuClass = isMenuOpen ? 'top-0' : '-top-full';
 
   return (
     <header
       className={`flex items-center justify-between py-4 px-10 border-b-1 border-black dark:border-white bg-white dark:bg-black sticky transition-top duration-500 ${visibleClass}`}
     >
-      <Link href="/" className="text-3-1/2xl text-black dark:text-white">
-        ✦
-      </Link>
       <div className="flex items-center">
-        <nav className="flex items-center gap-8">
-          <LinkButton href="/" title="Work" type="secondary" />
-          <LinkButton href="/about" title="About" type="secondary" />
+        <Link href="/" className="text-3-1/2xl text-black dark:text-white z-10">
+          ✦
+        </Link>
+      </div>
+      <div className="flex items-center z-10">
+        <nav className="hidden md:flex items-center gap-8">
+          {links.map(({href, title}) => (
+            <LinkButton key={href} href={href} title={title} type="secondary" />
+          ))}
         </nav>
-        <label className="flex items-center ml-20">
+        <label className="flex items-center md:ml-20">
           <input
             type="checkbox"
             className="sr-only peer"
             onClick={toggleTheme}
             defaultChecked={theme === 'dark'}
+            aria-label={theme === 'dark' ? 'Dark mode' : 'Light mode'}
           />
           <div className="cursor-pointer w-10 h-6 relative border rounded-2xl border-solid border-black dark:border-white after:absolute after:top-1/2 after:left-1 after:-translate-y-1/2 after:w-4 after:h-4 after:rounded-full after:bg-black dark:after:bg-white peer-checked:after:translate-x-5 peer-checked:after:left-auto after:transition-transform after:duration-500" />
         </label>
+
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className={`md:hidden ml-6 h-6 w-6 flex items-center before:bg-black after:bg-black before:rounded-sm after:rounded-sm dark:before:bg-white dark:after:bg-white before:w-6 after:w-6 before:h-3/16 after:h-3/16 before:absolute after:absolute  before:transition-toggle after:transition-toggle before:duration-500 after:duration-500 ${menuBtnClass}`}
+        />
       </div>
+
+      <nav
+        className={`md:hidden w-full max-h-full fixed top-0 bottom-0 left-0 right-0 bg-white dark:bg-black transition-top duration-500 ${mobileMenuClass}`}
+      >
+        <div className="flex flex-col mt-40 px-6">
+          {links.map(({href, title}) => (
+            <Link
+              key={href}
+              href={href}
+              className="font-bold text-5xl mb-6 text-black dark:text-white"
+            >
+              {title}
+            </Link>
+          ))}
+        </div>
+      </nav>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         '6-1/2xl': ['4rem', {lineHeight: '1'}], // 64px
       },
       spacing: {
+        '3/16': '0.1875rem', // 3px
         15: '3.75rem', // 60px
         30: '7.5rem', // 120px
         68: '17rem', // 272px
@@ -42,7 +43,11 @@ module.exports = {
       },
       transitionProperty: {
         top: `top, ${transitionProperty.colors}`,
+        toggle: `${transitionProperty.transform}, ${transitionProperty.colors}`,
       },
+      rotate: {
+        360: '360deg',
+      }
     },
   },
   plugins: [],
