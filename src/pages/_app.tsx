56806204d57169abdf38b9f06f17ab7782f96e06
Gonzalo Stoll <stollgonzalo@gmail.com>
2022-12-28 23:24:47 +0100

    refactor: Using the src directory

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,12 +9,12 @@ import '../styles/globals.css';
 const SctoGroteskFont = localFont({
   src: [
     {
-      path: '../public/fonts/Scto_Grotesk_A.woff2',
+      path: '../../public/fonts/Scto_Grotesk_A.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Scto_Grotesk_A_Bold.woff2',
+      path: '../../public/fonts/Scto_Grotesk_A_Bold.woff2',
       weight: '700',
       style: 'normal',
     },
