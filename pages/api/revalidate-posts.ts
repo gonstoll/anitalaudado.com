14e5a0881a609fddd86ab504cfa2646451212c3c
Gonzalo Stoll <stollgonzalo@gmail.com>
2022-12-28 20:21:54 +0100

    chore: Trying new env variables

--- pages/api/revalidate-posts.ts
+++ pages/api/revalidate-posts.ts
@@ -1,7 +1,7 @@
 import {isValidSignature, SIGNATURE_HEADER_NAME} from '@sanity/webhook';
 import type {NextApiRequest, NextApiResponse} from 'next';
 
-const secret = process.env.SANITY_WEBHOOK_SECRET!;
+const secret = process.env.SANITY_WEBHOOK_SECRET_POSTS!;
 
 export default async function handler(
   req: NextApiRequest,
